1f73c69d62698a4ab93618094354ec9985764132
nclvrl <nicolecdverial@gmail.com>
2025-03-18 17:38:09 +0800

    Added Profile model and modified Recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
 class Ingredient(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
@@ -12,7 +18,10 @@ class Ingredient(models.Model):
         return reverse('ledger:ingredient_detail')
 
 class Recipe(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -23,4 +32,4 @@ class Recipe(models.Model):
 class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
-    quantity = models.CharField(max_length = 255)                           
+    quantity = models.CharField(max_length=255)                           
