a8e6aae8577bbb63c4228fc04da450122b0b1442
nclvrl <nicolecdverial@gmail.com>
2025-03-18 18:12:43 +0800

    Fixed error regarding BaseUserAdmin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import *
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
 
-class UserAdmin(admin.BaseUserAdmin):
+class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInline]
 
 class RecipeIngredientInline(admin.TabularInline):
