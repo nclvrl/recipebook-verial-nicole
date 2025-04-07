a4e99556d00ed8fc8be6595903bdc5145d8f605c
nclvrl <nicolecdverial@gmail.com>
2025-04-07 17:49:15 +0800

    Added Ingredient and RecipeIngredient forms

--- ledger/forms.py
+++ ledger/forms.py
@@ -10,3 +10,15 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name']
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name']
+
+class RecipeIngredientForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = ['ingredient', 'quantity']
+    
+    ingredient = forms.ModelChoiceField(queryset=Ingredient.objects.all())
